3167591190b6720092a4bf3c9cb35b7c3e6388ec
April Howard <aprilhoward@Aprils-MBP.fios-router.home>
2020-08-31 15:42:02 -0400

    saving

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -6,22 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-//annotate as a controller so to tell Spring that this is a Spring
-//component. Because it's a Spring component we've given it a
-//constructor for the AuthorRepository. Spring uses dependency injection
-//to inject an instance of the author repository that has the CRUD operations.
-// then we add a user request mapping to the map request/url (or path)
-// of the authors. For that, we're asking Spring MVC to inject in an instance
-// of the model when it's called. We're taking that model and adding an
-// attribute of it called authors. We're getting an iterable from
-//the author repository which will give everything in the database,
-// and returning back the view (the identifier for the view,
-// which is "authors/list", and by default, SpringBoot is
-// configuring  thymeleaf to look at the templates directory.
-// In the template's directory, it looks under the path of authors,
-// and for the view called "list".
-//then we added in the list.html to display the author's
-//ID, firstName and lastName.
 @Controller
 public class AuthorController {
 
